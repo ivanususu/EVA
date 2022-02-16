d9f549f8e7091807a8438fc376b390d7d020f26d
ivanususu <ivan.jankuloski@gmail.com>
2022-02-16 23:30:35 +0100

    Add skeleton

---
+++ variabler.sh
@@ -0,0 +1,29 @@
+#!/bit/bash
+
+
+### Clone git repo
+
+git pull
+
+
+
+### Decrypt ansible env file
+
+ansible-vault decrypt 
+
+
+
+### Make the necesary change
+
+
+
+
+
+
+### Encrypt ansible env file
+
+ansible-vault encrypt 
+
+
+
+### Upload to git 
